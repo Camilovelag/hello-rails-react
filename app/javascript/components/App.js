30e4e828f116d11b27c8948aa5d4645523c0c691
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-20 18:18:22 -0500

    Create a Greeting component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Greeting from "./Greeting";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<h1>Hi from react router dom</h1>} />
+        <Route path="/" exact element={<Greeting/>} />
       </Routes>
     </Router>
     );}
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <h1>Hi from Greetings component</h1>
+  );
+}
+
+export default Greeting;
