8c9820e51e70931995773e1ea40b46141d98b833
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-20 19:21:51 -0500

    Display random msg on Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGreetings } from "../redux/greetings/greetingsSlice";
 
 const Greeting = () => {
+  let greetingMessage = useSelector((state) => state.greetings.message);
+  const dispatch = useDispatch();
+
   return (
-    <h1>Hi from Greetings component</h1>
+    <div>
+      <h3>My awesome random greetings board</h3><br/>
+      <strong>{ greetingMessage }</strong><br/><br/><br/>
+      <button type="button" onClick={() => dispatch(getGreetings())}>
+        Random message
+      </button>
+    </div>
   );
 }
 
--- app/javascript/redux/greetings/greetingsSlice.js
+++ app/javascript/redux/greetings/greetingsSlice.js
@@ -4,20 +4,20 @@ const getGreetings = createAsyncThunk('greetings/getGreetings',
   async () => {
     const response = await fetch('http://127.0.0.1:3000/api/v1/greetings');
     const data = await response.json();
-    return data;
+    return data[0].message;
   }
 );
 
 const greetingsReducer = createSlice({
   name: 'greetings',
   initialState: {
-    greeting: 'No greetings message.'
+    message: 'No greetings message.'
   },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getGreetings.fulfilled, (state, action) => ({
       ...state,
-      greeting: action.payload,
+      message: action.payload,
     }));
   },
 });
